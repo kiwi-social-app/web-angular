ffa06bfd46a33f61b010d8f0c1f57720bb8c5abe
Daniel <danielo.visage@gmail.com>
2025-01-12 11:00:18 +0100

    wip: add user to message

--- src/app/components/chat/chat.component.ts
+++ src/app/components/chat/chat.component.ts
@@ -1,27 +1,44 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Message } from 'src/app/models/message.model';
+import { AuthService } from 'src/app/services/auth.service';
 import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-chat',
   templateUrl: './chat.component.html',
-  styleUrls: ['./chat.component.scss']
+  styleUrls: ['./chat.component.scss'],
 })
 export class ChatComponent {
-  newMessage!: string;
-  messageList: string[] = [];
+  newMessage!: Message;
+  messageList: Message[] = [];
+  currentUser!: any;
+  form = new FormGroup({
+    messageBody: new FormControl('', Validators.required),
+    messageAuthor: new FormControl()
+  });
 
-  constructor(private chatService: ChatService){
-
+  constructor(private chatService: ChatService, public auth: AuthService) {
+    this.currentUser = this.auth.getCurrentUser();
   }
 
-  ngOnInit(){
-    this.chatService.getNewMessage().subscribe((message: string) => {
+  ngOnInit() {
+    this.auth.afAuth.authState.subscribe((user) => {
+      if (user) {
+        this.currentUser = user;
+      }
+    });
+    this.chatService.getNewMessage().subscribe((message) => {
+      console.log(message)
       this.messageList.push(message);
-    })
+    });
   }
 
   sendMessage() {
+    this.newMessage = {
+      body: this.form.controls.messageBody.value ?? "undefined",
+      author: this.currentUser,
+    };
     this.chatService.sendMessage(this.newMessage);
-    this.newMessage = '';
   }
 }
--- src/app/services/chat.service.ts
+++ src/app/services/chat.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { io } from "socket.io-client";
+import { Message } from '../models/message.model';
 
 
 @Injectable({
@@ -8,17 +9,19 @@ import { io } from "socket.io-client";
 })
 export class ChatService {
 
-  public message$: BehaviorSubject<string> = new BehaviorSubject('');
+  public message$: BehaviorSubject<Message> = new BehaviorSubject<Message>({body:'', author:{uid:'', email:''}});
   constructor() {}
 
   socket = io('http://localhost:4000');
 
-  public sendMessage(message : any) {
+  public sendMessage(message : Message) {
+    console.log(message)
     this.socket.emit('message', message);
   }
 
   public getNewMessage = () => {
     this.socket.on('message', (message) =>{
+      console.log(message)
       this.message$.next(message);
     });
 
