ce9a6d5cbf3463ed88bfc79701406941bf41a10c
Daniel <danielo.visage@gmail.com>
2025-01-12 11:00:18 +0100

    feat : add multiple file upload option

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -1,7 +1,5 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import {
-  AngularFireStorage,
   AngularFireUploadTask,
 } from '@angular/fire/compat/storage';
 import { Storage, ref, uploadBytesResumable } from '@angular/fire/storage';
@@ -21,12 +19,10 @@ export class FileUploadComponent {
   snapshot!: Observable<any>;
   downloadUrl!: Observable<string>;
 
-  constructor(private http: HttpClient) // private storage: AngularFireStorage
-  {}
-
   uploadFile(input: HTMLInputElement) {
     if (!input.files) return;
     const files: FileList = input.files;
+    console.log(files)
     for (let i = 0; i < files.length; i++) {
       const file = files.item(i);
       if (file) {
@@ -35,42 +31,4 @@ export class FileUploadComponent {
       }
     }
   }
-
-  // startUpload(event: FileList) {
-  //   const file = event.item(0);
-
-  //   if (file?.type.split('/')[0] !== 'image') {
-  //     console.error('Unsupported file type');
-  //     return;
-  //   }
-
-  //   const path = `test/${new Date().getTime()}_${file.name}`;
-
-  //   const customMetadata = { app: 'chatter app' };
-
-  //   this.task = this.storage.upload(path, file, { customMetadata });
-
-  //   this.percentage = this.task.percentageChanges();
-  //   this.snapshot = this.task.snapshotChanges();
-  //   // this.downloadUrl = this.task.downloadURL();
-  // }
-
-  // isActive(snapshot : any) {
-  //   return (
-  //     snapshot.state === 'running' &&
-  //     snapshot.bytesTransferred < snapshot.totalBytes
-  //   );
-  // }
-
-  // onFileSelected(event: any) {
-  //   this.selectedFile = <File>event.target.files[0];
-  // }
-
-  // onUpload() {
-  //   const fd = new FormData();
-  //   fd.append('image', this.selectedFile, this.selectedFile.name);
-  //   this.http.post('', fd).subscribe((res) => {
-  //     console.log(res);
-  //   });
-  // }
 }
