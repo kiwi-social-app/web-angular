42d2c3254d32bbe7936b5c76cec6ca71eea6bd2d
Daniel <danielo.visage@gmail.com>
2025-01-12 11:00:17 +0100

    feat: add createdAt date on post/comment renders

--- src/app/models/post.model.ts
+++ src/app/models/post.model.ts
@@ -1,8 +1,10 @@
+import { Timestamp } from "rxjs";
+
 export interface Post {
   id: string;
   title: string;
   body: string;
   image?: string | null;
   userID: string;
-  createdAt: Date;
+  createdAt: any;
 }
