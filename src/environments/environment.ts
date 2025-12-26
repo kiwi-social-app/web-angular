202b711915ee8d894316128a99d2bb6736a486e2
Daniel <danielo.visage@gmail.com>
2025-12-26 13:08:29 +0100

    migrate to new firebase project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,15 +7,13 @@ export const environment = {
   apiUrl: '/api',
   wsUrl: '/ws',
   firebase: {
-    projectId: 'chatter-app-33613',
-    appId: '1:854298776334:web:002b2a524c9c22f813bd5f',
-    databaseURL:
-      'https://chatter-app-33613-default-rtdb.europe-west1.firebasedatabase.app',
-    storageBucket: 'chatter-app-33613.appspot.com',
-    apiKey: 'AIzaSyAJk38s6u0T152P8tfGsmkG3sWpgps88-Q',
-    authDomain: 'chatter-app-33613.firebaseapp.com',
-    messagingSenderId: '854298776334',
-    measurementId: 'G-5MRS52FM7G',
+    apiKey: 'AIzaSyBa0jYj5WZhDU7O24ySqAoq1niJW0z-mX8',
+    authDomain: 'kiwi-social.firebaseapp.com',
+    projectId: 'kiwi-social',
+    storageBucket: 'kiwi-social.firebasestorage.app',
+    messagingSenderId: '448989396094',
+    appId: '1:448989396094:web:1827bc240298f9ccacac2f',
+    measurementId: 'G-LRLYL8BMM8',
   },
 };
 
